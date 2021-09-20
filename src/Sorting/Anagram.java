134b011c2da476b35415d4e1e524e8a6939a4ff2
Deepakkumar14 <deepakkumar.murugan.r.r@gmail.com>
2021-09-20 08:15:05 +0530

    edited in home

--- src/Sorting/Anagram.java
+++ src/Sorting/Anagram.java
@@ -5,18 +5,25 @@ import java.util.ArrayList;
 public class Anagram {
     public static void main(String[] args) {
         String str1="geeksforgeeks";
-        String str2="forgeeksgeek";
+        String str2="forgeeksgeeks";
         char array1[] = str1.toCharArray();
         char array2[] = str2.toCharArray();
         int[] count = new int[256];
-        int i;
-        for(i = 0; i < array1.length;)
+        for(int i = 0; i < array1.length;i++)
         {
             count[array1[i] - 'a']++;
             count[array2[i] - 'a']--;
         }
-            if(count.length==0) {
-                System.out.println("Anagram");
+        Boolean bool=true;
+        for(int i = 0; i < 256; i++){
+            if (count[i] != 0)
+            {
+                bool=false;
+            }
+    }
+
+            if(bool) {
+                System.out.println(" Anagram");
             }else {
                 System.out.println("Not Anagram");
             }
